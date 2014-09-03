1971ca62d41e1a3a870539c5e1b4025fdea4da7f
pystone <pystone9205@gmail.com>
2014-09-03 03:18:09 -0400

    demo version of TransactionalFileInputStream; move input.txt to its right place so that can be fetched with relative path

--- ProcessMigration/src/edu/cmu/andrew/ds/io/TransactionalFileInputStream.java
+++ ProcessMigration/src/edu/cmu/andrew/ds/io/TransactionalFileInputStream.java
@@ -1,8 +1,10 @@
 package edu.cmu.andrew.ds.io;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Serializable;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * To facilitate migrating processes with open files, you will need to implement a transactional I/O library.
@@ -21,17 +23,60 @@ import java.io.Serializable;
  *
  */
 
-public class TransactionalFileInputStream extends InputStream implements Serializable {
+public class TransactionalFileInputStream implements Serializable {
 	
 	/*
 	 * In order to improve performance, you can also choose to “cache” these connections by reusing them, 
 	 * unless a “migrated” flag is set, etc, in which case you would set the flag upon migration and reset
 	 * it any time a file handle is created or renewed.
 	 */
-	private boolean migrated;
+	
+	// TODO: 1. add mutex to ensure atomic; 2. more read interfaces
+	private boolean _migrated = false;
+	private FileChannel _in = null;
+	private long _pos = 0;
+	private Path _path = null;
 
-	@Override
-	public int read() throws IOException {
-		return 0;
+	
+	public TransactionalFileInputStream(String path) {
+		_path = Paths.get(path);
 	}
+	
+	public void setMigrated() {
+		_migrated = true;
+	}
+	
+	public void resetMigrated() {
+		_migrated = false;
+	}
+	
+	// read in one byte
+	public int read() throws IOException {
+		ByteBuffer buf = ByteBuffer.allocate(1);
+		
+		try {
+			if (_in == null) {
+				System.out.println("open file: "+_path.toString());
+				_in = FileChannel.open(_path);
+				_in = _in.position(_pos);
+			}
+			
+			_in.read(buf);	// TODO: handle read in error
+			
+			/* After reading in, should use this method to get buffer ready to write */
+			buf.flip();
+			
+			_pos = _in.position();
+			if (_migrated) {
+				System.out.println("file closed");
+				_in.close();
+				_in = null;
+			}
+			return buf.get();
+			
+		}finally {
+			
+		}
+	}
+	
 }
--- ProcessMigration/src/edu/cmu/andrew/ds/main/PMMain.java
+++ ProcessMigration/src/edu/cmu/andrew/ds/main/PMMain.java
@@ -1,5 +1,9 @@
 package edu.cmu.andrew.ds.main;
 
+import java.io.IOException;
+
+import edu.cmu.andrew.ds.io.*;
+
 /**
  * Starting point of the whole project.
  * 
@@ -13,6 +17,18 @@ public class PMMain {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Nothing has been done now!");
+		
+		// change the file name to absolute path
+		TransactionalFileInputStream a = new TransactionalFileInputStream("input.txt");
+		try {
+			a.setMigrated();
+			System.out.println(String.valueOf(a.read()));
+			System.out.println(String.valueOf(a.read()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
 
 }
